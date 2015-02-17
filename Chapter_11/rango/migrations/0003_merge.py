ea9cfbc60d5fb8568abf4d65df273feb3a896c8e
unknown <2103291s@BO706-04.ad.dcs.gla.ac.uk>
2015-02-17 14:17:09 +0000

    no message

---
+++ Chapter_11/rango/migrations/0003_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('rango', '0002_remove_page_slug'),
+        ('rango', '0002_auto_20150127_2123'),
+    ]
+
+    operations = [
+    ]
